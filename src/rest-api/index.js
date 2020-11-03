477cfb19bd38e30205bfbbc4a323942e366eebd7
Royer <royer01gomez@gmail.com>
2020-11-03 19:24:04 -0400

    fix port

--- src/rest-api/index.js
+++ src/rest-api/index.js
@@ -19,12 +19,15 @@ app.use('/', express.static('src/public/image')) //for render favicon
 //Configure route
 require('./routes')(app)
 var { Helper } = require('./utils');
+const helper = require('./utils/Helper');
 
 module.exports = {
     listen: () => {
         var httpServer = http.createServer(app);
-        httpServer.listen(Helper.PORT, () => {
-            console.log(`RestApi running at ${Helper.HTTPUrl}`);
+        const port = process.env.port || 3000
+        httpServer.listen(port, () => {
+            console.log(`RestApi running`);
+            // console.log(`RestApi running at ${Helper.HTTPUrl}`);
         })
     }
 }
