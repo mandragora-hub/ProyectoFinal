dafe635960c83d248c5420c8c408cc55a82dbd27
Royer <royer01gomez@gmail.com>
2020-11-03 16:54:50 -0400

    Added firestore support

--- src/rest-api/routes/Iso815Route.js
+++ src/rest-api/routes/Iso815Route.js
@@ -1,11 +1,72 @@
 const express = require('express');
+
+///firebase
+const admin = require('firebase-admin');
+const serviceAccount = require('../cert/iso-815-e60758a0d86a.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+////////
+
+
 module.exports = function (options = {}) {
     const { interface } = options
     const router = express.Router();
 
     const prefix = 'iso815'
-    router.get(`/${prefix}`, async (req, res, next) => {
-        // All route here
+    router.get(`/${prefix}/exchange/:currency`, async (req, res, next) => {
+        const currency = req.params.currency;
+        if (currency) {
+            const doc = await db.collection('exchange rate').doc(currency).get();
+            if (!doc.exists) {
+                return res.json('No such document')
+            } else {
+                return res.json(doc.data());
+            }
+        }
+        res.json('currency is not defined') 
+    });
+
+    router.get(`/${prefix}/financial/:id`, async (req, res, next) => {
+        const id = req.params.id;
+        if (id) {
+            const doc = await db.collection('financial health').doc(id).get();
+            if (!doc.exists) {
+                return res.json('No such document')
+            } else {
+                return res.json(doc.data());
+            }
+        }
+        res.json('financial is not defined') 
+    });
+
+    router.get(`/${prefix}/inflation/:date`, async (req, res, next) => {
+        const date = req.params.date;
+        if (date) {
+            const doc = await db.collection('inflation index').doc(date).get();
+            if (!doc.exists) {
+                return res.json('No such document')
+            } else {
+                return res.json(doc.data());
+            }
+        }
+        res.json('date index is not defined') 
+    });
+
+    router.get(`/${prefix}/service/:id`, async (req, res, next) => {
+        const id = req.params.id;
+        if (id) {
+            const doc = await db.collection('web service use').doc(id).get();
+            if (!doc.exists) {
+                return res.json('No such document')
+            } else {
+                return res.json(doc.data());
+            }
+        }
+        res.json('id is not defined') 
     });
 
     return router;
--- src/rest-api/routes/MenuRoute.js
+++ src/rest-api/routes/MenuRoute.js
@@ -3,12 +3,14 @@ const express = require('express');
 const { Helper } = require('../utils')
 module.exports = function (options = {}) {
     const router = express.Router();
+
+    const prefix = 'iso815'
     router.get('/', (req, res) => {
         const menu = {
-            obtener_tasa_de_cambio: `${Helper.HTTPUrl}/iso815`,
-            consultar_el_indice_de_inflacion: `${Helper.HTTPUrl}/iso815`,
-            consultar_la_salud_financiera: `${Helper.HTTPUrl}/iso815`, 
-            consulta_uso_de_web_service: `${Helper.HTTPUrl}/iso815`
+            obtener_tasa_de_cambio: `${Helper.HTTPUrl}/${prefix}/exchange/{currency}`,
+            consultar_el_indice_de_inflacion: `${Helper.HTTPUrl}/${prefix}/inflation/{date}`,
+            consultar_la_salud_financiera: `${Helper.HTTPUrl}/${prefix}/financial/{id}`,
+            consulta_uso_de_web_service: `${Helper.HTTPUrl}/${prefix}/service/{id}`,
         }
         res.json(menu)
     });
